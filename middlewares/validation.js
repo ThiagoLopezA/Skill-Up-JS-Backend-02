bea5a6618ad01a73e5f009b697dcad3680ced448
Gustavo Tolay <115080196+SlasherF4@users.noreply.github.com>
2022-12-01 19:05:37 -0300

    Validation function (#2)
    * feat(middlewares): add validation function
    
    * feat(schemas): add user schema
    
    * feat(schemas): add category schema
    
    * feat(schemas): add transaction schema

---
+++ middlewares/validation.js
@@ -0,0 +1,15 @@
+const { checkSchema, validationResult } = require('express-validator');
+const { ErrorObject } = require('../helpers/error');
+
+const validation = (schema) => [
+  checkSchema(schema, ['body']),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new ErrorObject('Invalid value/s', 422, errors.mapped());
+    }
+    return next();
+  },
+];
+
+module.exports = validation;
