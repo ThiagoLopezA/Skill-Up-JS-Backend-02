7d7a4931309b790b2b62531a1f4fcfd851164654
Thiago López <thiagolopezalderete@hotmail.com>
2022-12-01 09:33:06 -0300

    <feat>[service]: categories service created & category model fixed

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,5 +1,23 @@
 const createHttpError = require("http-errors");
 const { endpointResponse } = require("../helpers/success");
 const { catchAsync } = require("../helpers/catchAsync");
+const { getOne } = require("../services/categories.service");
 
-module.exports = {};
+module.exports = {
+  getOne: catchAsync(async (req, res, next) => {
+    try {
+      const response = await getOne(req.params.id);
+      endpointResponse({
+        res,
+        message: "Category retrieved successfully",
+        body: response,
+      });
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error retrieving category] - [/:id - GET]: ${error.message}`
+      );
+      next(httpError);
+    }
+  }),
+};
