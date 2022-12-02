4e1bbd4d96efa45865f74446a216d30f2736bd0d
Gustavo Tolay <jeremiastolay1714@gmail.com>
2022-12-02 14:00:39 -0300

    feat(services): add deleteOne service

--- services/categories.service.js
+++ services/categories.service.js
@@ -10,3 +10,16 @@ exports.getOne = async id => {
     throw new ErrorObject(error.message, error.statusCode || 500);
   }
 };
+
+exports.deleteOne = async id => {
+  try {
+    const category = await Category.findByPk(id);
+    if (category !== null) {
+      const destroy = await Category.destroy({ where: { id } });
+      return category
+    }
+    throw new ErrorObject("Category not found", 404);
+  } catch (error) {
+    throw new ErrorObject(error.message, error.statusCode || 500);
+  }
+};
