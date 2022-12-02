9e6851f12d7f64da109c42c84a9232cebbac7e4b
Gustavo Tolay <jeremiastolay1714@gmail.com>
2022-12-01 21:15:38 -0300

    feat(controllers): add user delete controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,24 +1,41 @@
-const createHttpError = require('http-errors')
-const { User } = require('../database/models')
-const { endpointResponse } = require('../helpers/success')
-const { catchAsync } = require('../helpers/catchAsync')
+const createHttpError = require('http-errors');
+const { deleteOne } = require('../services/users.service');
+const { endpointResponse } = require('../helpers/success');
+const { catchAsync } = require('../helpers/catchAsync');
 
 // example of a controller. First call the service, then build the controller method
 module.exports = {
   get: catchAsync(async (req, res, next) => {
     try {
-      const response = await User.findAll()
+      const response = await User.findAll();
       endpointResponse({
         res,
         message: 'Users retrieved successfully',
         body: response,
-      })
+      });
     } catch (error) {
       const httpError = createHttpError(
         error.statusCode,
-        `[Error retrieving users] - [index - GET]: ${error.message}`,
-      )
-      next(httpError)
+        `[Error retrieving users] - [index - GET]: ${error.message}`
+      );
+      next(httpError);
     }
   }),
-}
+  deleteOne: catchAsync(async (req, res, next) => {
+    try {
+      const id = req.params.id;
+      const response = await deleteOne(id);
+      endpointResponse({
+        res,
+        message: 'User deleted successfully',
+        body: response,
+      });
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error deleting user] - [/:id - DELETE]: ${error.message}`
+      );
+      next(httpError);
+    }
+  }),
+};
