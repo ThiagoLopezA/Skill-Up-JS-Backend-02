ba35270cda43acc491eb143c3654d6decf7d419e
Gustavo Tolay <jeremiastolay1714@gmail.com>
2022-12-01 21:18:47 -0300

    fix(models): fixed user model

--- database/models/user.js
+++ database/models/user.js
@@ -19,9 +19,6 @@ module.exports = (sequelize, DataTypes) => {
     email: DataTypes.STRING,
     avatar: DataTypes.STRING,
     roleId: DataTypes.INTEGER,
-    deletedAt: {
-      type: DataTypes.STRING
-    },
   }, {
     sequelize,
     timestamps: true,
