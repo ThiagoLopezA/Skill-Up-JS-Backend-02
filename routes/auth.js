f3041fc8a1e5bcb4771a3647f84fe8ceb77762a8
Thiago López <thiagolopezalderete@hotmail.com>
2022-12-01 21:01:43 -0300

    <feat>(routes): adding validations to the login route

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const { login } = require("../controllers/auth");
+const validation = require("../middlewares/validation");
+const { email, password } = require("../schemas/user");
 const router = express.Router();
 
-router.post("/login", login);
+router.post("/login", validation({ email, password }), login);
 
 module.exports = router;
