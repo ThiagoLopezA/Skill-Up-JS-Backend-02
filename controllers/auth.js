8d3f577bb0be0c3e0bec41e8f14b693736f7337b
Thiago López <thiagolopezalderete@hotmail.com>
2022-12-01 21:07:43 -0300

    <feat>[services]: added getByEmail method to user service

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,12 @@
 const createHttpError = require("http-errors");
 const { endpointResponse } = require("../helpers/success");
 const { catchAsync } = require("../helpers/catchAsync");
+const JWT = require("../helpers/jwt.helper");
 
 module.exports = {
   login: catchAsync(async (req, res, next) => {
     try {
+      const { email, password } = req.body;
     } catch (error) {
       const httpError = createHttpError(
         error.statusCode,
