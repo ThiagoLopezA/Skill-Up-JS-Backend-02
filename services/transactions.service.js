7c8923c510c0752f09516546fa152da91f3c7e41
Irupe <lilianirupemeza@gmail.com>
2022-12-02 07:31:52 -0300

    <feat>[services] add new transaction

---
+++ services/transactions.service.js
@@ -0,0 +1,19 @@
+const { ErrorObject } = require("../helpers/error");
+const { Transaction } = require("../database/models");
+
+exports.createOne = async (props) => {
+  try {
+    if (!props.userId || !props.amount || !props.date || !props.date) {
+      throw new ErrorObject("Incomplete data", 404);
+    }
+    const newTransaction = await Transaction.create({
+      amount: props.amount,
+      date: props.date,
+      userId: props.userId,
+      categoyId: props.categoryId,
+    });
+    return newTransaction;
+  } catch (error) {
+    throw new ErrorObject(error.message, error.statusCode || 500);
+  }
+};
