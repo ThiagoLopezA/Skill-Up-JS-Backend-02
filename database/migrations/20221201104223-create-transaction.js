b12b77d577105a3e71e8b286a1074a06f54a63aa
Irupe <lilianirupemeza@gmail.com>
2022-12-02 08:18:25 -0300

    <fix>[] transactions description allownull

--- database/migrations/20221201104223-create-transaction.js
+++ database/migrations/20221201104223-create-transaction.js
@@ -9,7 +9,7 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       description: {
-        allowNull: false,
+        allowNull: true,
         type: Sequelize.STRING
       },
       amount: {
