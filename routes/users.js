559395909e2caa9fa6a56e26ae750685fc504ddd
Pabpol <pabpolval@gmail.com>
2022-12-02 19:29:46 -0300

    feat(routes): add validation to create user route

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const { get, getOne, deleteOne, create } = require("../controllers/users");
-
+const {  first_name, last_name ,email, password } = require("../schemas/user");
+const validation = require("../middlewares/validation");
 const router = express.Router();
 
 router.get("/", get);
 router.get("/:id", getOne);
 
 router.delete('/:id', deleteOne);
-router.post("/", create)
+router.post("/", validation({first_name, last_name, email, password }), create)
 
 module.exports = router;
