de952f71305b97dc2d839bd3461259a911e6cb08
Pabpol <pabpolval@gmail.com>
2022-12-02 19:04:22 -0300

    feat(controller): add create user controller

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,9 @@ const createHttpError = require("http-errors");
 const { User } = require("../database/models");
 const { endpointResponse } = require("../helpers/success");
 const { catchAsync } = require("../helpers/catchAsync");
-const { getUser, deleteOne } = require("../services/users.service");
+const { getUser, deleteOne, createUser } = require("../services/users.service");
+const bcrypt = require("../helpers/bcrypt.helper");
+
 
 // example of a controller. First call the service, then build the controller method
 module.exports = {
@@ -51,6 +53,25 @@ module.exports = {
       const httpError = createHttpError(
         error.statusCode,
         `[Error retrieving user] - [/:id - GET]: ${error.message}`
+      );
+      next(httpError);
+    }
+  }),
+  create: catchAsync(async (req, res, next) => {
+    try {
+      const user = req.body;
+      user.password = await bcrypt.hash(req.body.password)
+      console.log(user)
+      const response = await createUser(user);
+      endpointResponse({
+        res,
+        message: "User created successfully",
+        body: response,
+      });
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error creting user] - [POST]: ${error.message}`
       );
       next(httpError);
     }
