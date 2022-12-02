b03d9b8859ae73308ede00bda5247a94cea2bba8
Evange-Fernandez <fernandez.eva.tdf@gmail.com>
2022-12-01 21:26:06 -0300

    <feat>Â[routes]: show user data route added

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,9 @@
-const express = require('express')
-const {
-   get,
-} = require('../controllers/users')
+const express = require("express");
+const { get, getOne } = require("../controllers/users");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', get)
+router.get("/", get);
+router.get("/:id", getOne);
 
-
-module.exports = router
+module.exports = router;
