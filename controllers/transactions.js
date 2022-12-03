9a92c7928728dc6eb7291697dd440925d6910e68
Evange-Fernandez <fernandez.eva.tdf@gmail.com>
2022-12-03 19:39:48 -0300

    <feat>Endpoint-show transaction data-route added

---
+++ controllers/transactions.js
@@ -0,0 +1,23 @@
+const createHttpError = require("http-errors");
+const { endpointResponse } = require("../helpers/success");
+const { catchAsync } = require("../helpers/catchAsync");
+const { getOne } = require("../services/transactions.service");
+
+module.exports = {
+  getOne: catchAsync(async (req, res, next) => {
+    try {
+      const response = await getOne(req.params.id);
+      endpointResponse({
+        res,
+        message: "Transaction retrieved successfully",
+        body: response,
+      });
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error retrieving category] - [/:id - GET]: ${error.message}`
+      );
+      next(httpError);
+    }
+  }),
+};
