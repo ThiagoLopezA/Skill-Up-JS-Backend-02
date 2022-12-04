6ae53bde66d64e0ae90ccc7e83a3a1fd25aef57b
Irupe <lilianirupemeza@gmail.com>
2022-12-04 17:49:42 -0300

    <feat>[routes]add route to edit user

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { get, getOne, deleteOne } = require("../controllers/users");
+const { get, getOne, deleteOne, editUser } = require("../controllers/users");
 
 const router = express.Router();
 
@@ -7,5 +7,7 @@ router.get("/", get);
 router.get("/:id", getOne);
 
 router.delete('/:id', deleteOne);
+
+router.put('/:id', editUser);
 
 module.exports = router;
