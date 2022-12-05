498f157ac646013b5dfc749aff6763972bb5cd2e
Evange-Fernandez <fernandez.eva.tdf@gmail.com>
2022-12-05 09:38:19 -0300

    <feat>[seeders]: Users seeders created

---
+++ database/seeders/20221204000815-create-users.js
@@ -0,0 +1,113 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const users = [
+      {
+        firstName: "Carlos",
+        lastName: "Sosa",
+        email: "carlos@mail.com",
+        avatar: "1.img",
+        password:"Hola1234",
+        roleId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        firstName: "Marta",
+        lastName: "Casas",
+        email: "marta@mail.com",
+        avatar: "1.img",
+        password:"Hola1234",
+        roleId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        firstName: "Juan",
+        lastName: "Perez",
+        email: "Juan@mail.com",
+        avatar: "1.img",
+        password:"Hola1234",
+        roleId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        firstName: "Maria",
+        lastName: "Gonzalez",
+        email: "maria@mail.com",
+        avatar: "1.img",
+        password:"Hola1234",
+        roleId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        firstName: "Juan",
+        lastName: "Fernandez",
+        email: "juan@mail.com",
+        avatar: "1.img",
+        password:"Hola1234",
+        roleId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        firstName: "Ana",
+        lastName: "Rios",
+        email: "ana@mail.com",
+        avatar: "1.img",
+        password:"Hola1234",
+        roleId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        firstName: "Marcos",
+        lastName: "Oviedo",
+        email: "marcos@mail.com",
+        avatar: "1.img",
+        password:"Hola1234",
+        roleId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        firstName: "Cristina",
+        lastName: "Ramos",
+        email: "cristina@mail.com",
+        avatar: "1.img",
+        password:"Hola1234",
+        roleId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        firstName: "Pedro",
+        lastName: "Lagos",
+        email: "pedro@mail.com",
+        avatar: "1.img",
+        password:"Hola1234",
+        roleId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        firstName: "Karina",
+        lastName: "Garcia",
+        email: "karina@mail.com",
+        avatar: "1.img",
+        password:"Hola1234",
+        roleId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ];
+    return await queryInterface.bulkInsert("Users", users, {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return await queryInterface.bulkDelete("Users", null, {});
+  },
+};
