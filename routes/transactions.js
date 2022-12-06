358c42e8055b9e2f5538cadbd7b343ed08cb17c3
Irupe <lilianirupemeza@gmail.com>
2022-12-05 21:15:52 -0300

    <featr>[routes] adding middelware

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const { getOne, deleteOne, createOne, getAllUserTransactions } = require("../controllers/transactions");
+const validation = require("../middlewares/validation")
+const {amount, date, category, user} = require("../schemas/transaction")
 
 const router = express.Router();
 
 router.get("/:id", getOne);
 
 router.delete("/:id", deleteOne)
-router.post("/", createOne);
+router.post("/",  validation({amount, date}), createOne);
 router.get("/", getAllUserTransactions);
 
 module.exports = router;
