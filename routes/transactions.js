0fa51198262fe6be1a92b98f1087979d9db3b595
Gustavo Tolay <jeremiastolay1714@gmail.com>
2022-12-07 20:59:37 -0300

    pull

--- routes/transactions.js
+++ routes/transactions.js
@@ -2,12 +2,14 @@ const express = require("express");
 const { getOne, deleteOne, createOne, getAllUserTransactions } = require("../controllers/transactions");
 const validation = require("../middlewares/validation")
 const {amount, date, category, user} = require("../schemas/transaction")
+const { ownership } = require("../middlewares/ownership");
+const authentication = require("../middlewares/authentication");
 
 const router = express.Router();
 
-router.get("/:id", getOne);
+router.get("/:id", authentication, getOne);
 
-router.delete("/:id", deleteOne)
+router.delete("/:id", ownership, deleteOne)
 router.post("/",  validation({amount, date}), createOne);
 router.get("/", getAllUserTransactions);
 
