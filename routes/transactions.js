ef6ef357dfc825e9c65c38276e5f2fad9ccaf359
Irupe <lilianirupemeza@gmail.com>
2022-12-07 17:01:26 -0300

    <fix>[routes] changing name in routes

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getOne, deleteOne, createOne, getAllUserTransactions } = require("../controllers/transactions");
+const { getOne, deleteOne, createOne, getAllTransactions } = require("../controllers/transactions");
 const validation = require("../middlewares/validation")
 const {amount, date, category, user} = require("../schemas/transaction")
 
@@ -9,6 +9,6 @@ router.get("/:id", getOne);
 
 router.delete("/:id", deleteOne)
 router.post("/",  validation({amount, date}), createOne);
-router.get("/", getAllUserTransactions);
+router.get("/", getAllTransactions);
 
 module.exports = router;
