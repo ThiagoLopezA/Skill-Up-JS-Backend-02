2702fd0d58a2b4d18b6ffb2138499b650a0f2d43
Thiago López <thiagolopezalderete@hotmail.com>
2022-12-06 21:15:23 -0300

    combine the 2 user services (repeated)

--- services/users.service.js
+++ services/users.service.js
@@ -11,11 +11,21 @@ exports.getUser = async id => {
   }
 };
 
-module.exports.deleteOne = async id => {
+exports.deleteOne = async id => {
   try {
     const user = await User.findByPk(id);
     if (!user) throw new ErrorObject("User not found", 404);
     await User.destroy({ where: { id } });
+  } catch (error) {
+    throw new ErrorObject(error.message, error.statusCode || 500);
+  }
+};
+
+exports.getByEmail = async email => {
+  try {
+    const user = await User.findOne({ where: { email } });
+    if (!user) throw new ErrorObject("User not found", 404);
+    return user;
   } catch (error) {
     throw new ErrorObject(error.message, error.statusCode || 500);
   }
