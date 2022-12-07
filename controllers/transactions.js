267e8c39322e93992619857eff2a2d165ff3496b
Thiago López <thiagolopezalderete@hotmail.com>
2022-12-07 12:27:44 -0300

    <feat>(controllers): transactions delete method returns token

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -31,10 +31,11 @@ module.exports = {
     try {
       const id = req.params.id;
       const response = await deleteOne(id);
+      const encrypted = jwt.encode(response, "1m");
       endpointResponse({
         res,
         message: "Transaction deleted successfully",
-        body: response,
+        body: encrypted,
       });
     } catch (error) {
       const httpError = createHttpError(
