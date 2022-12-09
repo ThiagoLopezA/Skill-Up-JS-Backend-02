a355ddb877ec93c4870d1197b32ae7d27ad789bb
Evangelina Fernández <99969703+evita-fernan@users.noreply.github.com>
2022-12-09 14:36:19 -0300

    <feat>[test]: Testing users (#33)
    * <feat>[test]: Testing users
    
    * <feat>[test]:Auth login route
    
    * <fix>[test]: Seeders for test
    
    * <feat>[test]:Update user route
    
    * <feat>[test]:Update user route

---
+++ test/user.test.js
@@ -0,0 +1,121 @@
+const request = require("supertest");
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const app = require("../app");
+const { expect } = require("chai");
+
+//Assertion Style
+chai.should();
+chai.use(chaiHttp);
+
+describe("GET /users/:id", () => {
+  /**
+   * Testing the GET (by id) route
+   */
+  it("respond with json containing a user", (done) => {
+    const userId = 1;
+    chai
+      .request(app)
+      .get("/users/" + userId)
+      .end((err, response) => {
+        response.should.have.status(200);
+        done();
+      });
+  });
+
+  it("respond with HTML Error `User not found` when the user does not exist", (done) => {
+    chai
+      .request(app)
+      .get("/users/" + 101)
+      .end((err, response) => {
+        response.should.have.status(404);
+        response.should.have.header("content-type", "text/html; charset=utf-8");
+        done();
+      });
+  });
+});
+
+/**
+ * Testing the POST user route
+ */
+describe("POST /users", (done) => {
+  it("respond with `User created successfully`", (done) => {
+    const data = {
+      first_name: "Edna",
+      last_name: "Krabappel",
+      email: "edna@mail.com",
+      password: "Hola1234",
+    };
+    chai
+      .request(app)
+      .post("/users")
+      .send(data)
+      .end((err, response) => {
+        response.should.have.status(200);
+        response.should.have.header("content-type", /json/);
+        done();
+      });
+  });
+});
+
+/**
+ * Testing the POST auth route
+ */
+
+describe("POST /auth", (done) => {
+  it("respond with `Authenticated successfully`", (done) => {
+    const data = {
+      email: "edna@mail.com",
+      password: "Hola1234",
+    };
+    chai
+      .request(app)
+      .post("/auth/login")
+      .send(data)
+      .end((err, response) => {
+        response.should.have.status(200);
+        response.should.have.header("content-type", /json/);
+        done();
+      });
+  });
+});
+
+/**
+ * Testing the PUT route
+ */
+
+describe("PUT /users/:id", (done) => {
+  it("respond with `User update successfully`", (done) => {
+    const userId = 1;
+    const data = {
+      firstName: "Marjorie",
+    };
+    chai
+      .request(app)
+      .put("/users/" + userId)
+      .send(data)
+      .end((err, response) => {
+        response.should.have.status(200);
+        response.should.have.header("content-type", /json/);
+        done();
+      });
+  });
+});
+
+/**
+ * Testing the DELETE route
+ */
+
+describe("DELETE /users/:id", (done) => {
+  it("respond with `User deleted successfully`", (done) => {
+    const userId = 2;
+    chai
+      .request(app)
+      .delete("/users/" + userId)
+      .end((err, response) => {
+        response.should.have.status(200);
+        response.should.have.header("content-type", /json/);
+        done();
+      });
+  });
+});
