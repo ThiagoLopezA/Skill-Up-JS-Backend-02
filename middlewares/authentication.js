a6c3f2acbd2aebd3a96bd0c215eae7af4199f84c
Gustavo Tolay <jeremiastolay1714@gmail.com>
2022-12-08 21:00:49 -0300

    feat(middlewares): add verify jwt function

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,4 +1,4 @@
-const { decode } = require("../helpers/jwt.helper");
+const { decode, verify } = require("../helpers/jwt.helper");
 const { ErrorObject } = require("../helpers/error");
 const { findUser } = require("../services/users.service");
 const { catchAsync } = require("../helpers/catchAsync");
@@ -7,9 +7,10 @@ const authentication = catchAsync(async (req, res, next) => {
   try {
     const token = req.headers["authorization"].split(" ")[1];
     const decoded = decode(token);
+    const verified = verify(token);
     const { iat, exp, ...user } = decoded;
     const find = await findUser(user);
-    if (!find) throw new ErrorObject("Invalid token", 403);
+    if (!find || !verified) throw new ErrorObject("Invalid token", 403);
     return next();
   } catch (error) {
     throw new ErrorObject(error.message, error.statusCode || 500);
