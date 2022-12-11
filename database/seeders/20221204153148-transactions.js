72120c7436c8e19973a8442bc9f19a65e96f76b0
Thiago López <thiagolopezalderete@hotmail.com>
2022-12-11 19:35:12 -0300

    fix(seeders): transactions seeders fixed

--- database/seeders/20221204153148-transactions.js
+++ database/seeders/20221204153148-transactions.js
@@ -1,32 +1,24 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return await queryInterface.bulkInsert("Transactions", [
       {
-        description:"Some description",
-        amount:500.10,
-        date:new Date(),
-        userId:1,
-        categoryId:1,
-        createdAt: new Date(),
-        updatedAt: new Date(),
+        description: "Some description",
+        amount: 500.1,
+        userId: 1,
+        categoryId: 1,
       },
       {
-        description:"Some diferent description",
-        amount:500.10,
-        date:new Date(),
-        userId:2,
-        categoryId:2,
-        createdAt: new Date(),
-        updatedAt: new Date(),
+        description: "Some diferent description",
+        amount: 500.1,
+        userId: 2,
+        categoryId: 2,
       },
     ]);
   },
 
   down: async (queryInterface, Sequelize) => {
-
     return await queryInterface.bulkDelete("Transactions", null, {});
-
-  }
+  },
 };
