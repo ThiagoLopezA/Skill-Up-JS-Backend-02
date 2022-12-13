d03c45893f888b3d78d10783c335f7ee862034ee
Tomás Salina <salinatomass53@gmail.com>
2022-12-13 20:39:22 -0300

    <fix>(login): remove authentication middleware

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,9 @@
-const express = require("express");
-const { login } = require("../controllers/auth");
-const validation = require("../middlewares/validation");
-const authentication = require("../middlewares/authentication")
-const { email, password } = require("../schemas/user");
-const router = express.Router();
+const express = require('express')
+const { login } = require('../controllers/auth')
+const validation = require('../middlewares/validation')
+const { email, password } = require('../schemas/user')
+const router = express.Router()
 
-router.post("/login", validation({ email, password }), authentication, login);
+router.post('/login', validation({ email, password }), login)
 
-module.exports = router;
+module.exports = router
