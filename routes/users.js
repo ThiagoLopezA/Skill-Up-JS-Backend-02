83827de4a98cd27a9df351d967d51ae9f2107459
Evangelina Fernández <99969703+evita-fernan@users.noreply.github.com>
2022-12-13 09:25:40 -0300

    <fix>[test]:authentication in user test (#38)

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,24 @@
 const express = require("express");
-const { get, getOne, deleteOne, editUser,  create } = require("../controllers/users");
-const {  first_name, last_name ,email, password } = require("../schemas/user");
+const {
+  get,
+  getOne,
+  deleteOne,
+  editUser,
+  create,
+} = require("../controllers/users");
+const { firstName, lastName, email, password } = require("../schemas/user");
 const validation = require("../middlewares/validation");
+const authentication = require("../middlewares/authentication");
 const router = express.Router();
 
-router.get("/", get);
-router.get("/:id", getOne);
-router.delete('/:id', deleteOne);
-router.post("/", validation({first_name, last_name, email, password }), create)
-router.put('/:id',  editUser);
+router.get("/", authentication, get);
+router.get("/:id", authentication, getOne);
+router.delete("/:id", authentication, deleteOne);
+router.post(
+  "/register",
+  validation({ firstName, lastName, email, password }),
+  create
+);
+router.put("/:id", authentication, editUser);
 
 module.exports = router;
